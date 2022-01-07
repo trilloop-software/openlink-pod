066c571ccf6c90341765b3ead32e90cfe31f32ab
Matt Holden <matt@hold3n.ca>
2022-01-06 20:26:33 -0500

    remote_conn_svc cleanup
    - clean up of remote_conn_svc, structuring for easier integration with other services
    - limiting to one remote client connected at a time to simplify design temporarily
    - added keep_alive parameter to configuration to keep connection open (and eventual necessity to detect if a client is disconnected)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
 use anyhow::Result;
 use std::sync::Arc;
-use tokio::{spawn, sync::{mpsc, watch, Mutex}, task::spawn_blocking};
+use tokio::{spawn, sync::{broadcast, mpsc, Mutex}};
 
 mod remote_conn_svc;
 mod packet;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let pod_state = Arc::new(Mutex::new(PodState::new()));
-    // mpsc - multi producer, single consumer
-    // feed commands from services to remote_conn_svc
-    let (remote_in_tx, mut remote_in_rx) = mpsc::channel::<()>(32);
-    // watch - single producer, mutli consumer
-    // feed commands received from remote_conn_svc to corresponding services
-    let (remote_out_tx, mut remote_out_rx) = watch::channel(());
+    let (remote_in_tx, remote_in_rx) = mpsc::channel::<i32>(32);
+    let (remote_out_tx, remote_out_rx) = mpsc::channel::<i32>(32);
+
+    let remote_conn_svc = remote_conn_svc::RemoteConnSvc { rx: remote_in_rx, tx: remote_out_tx };
 
     // spawn all services as tasks, and send corresponding control signals
-    spawn(remote_conn_svc::server(remote_in_rx, remote_out_tx));
+    spawn(remote_conn_svc.run());
 
     loop {
 
     }
 }
-
-/*struct PodState {
-
-}
-
-impl PodState {
-    fn new() -> Self {
-        PodState {
-            
-        }
-    }
-}*/
--- src/remote_conn_svc.rs
+++ src/remote_conn_svc.rs
@@ -4,106 +4,97 @@ use futures_util::stream::StreamExt;
 use quinn::{Endpoint, ServerConfig};
 use std::{error::Error, sync::Arc};
 use tracing::{error, info};
-use tokio::sync::{mpsc::Receiver, watch::Sender};
+use tokio::sync::{mpsc::Receiver, mpsc::Sender};
 
 use super::packet::*;
 
-pub async fn server(rx: Receiver<()>, tx: Sender<()>) -> Result<()> {
-    let code = {
-        if let Err(e) = run(rx, tx).await {
-            eprintln!("ERROR: {}", e);
-            1
-        } else {
-            0
-        }
-    };
-    ::std::process::exit(code);
+pub struct RemoteConnSvc {
+    pub rx: Receiver<i32>,
+    pub tx: Sender<i32>,
 }
 
-pub async fn run(rx: Receiver<()>, tx: Sender<()>) -> Result<()> {
-    let server_addr = "127.0.0.1:6007".parse().unwrap();
+impl RemoteConnSvc {
+    pub async fn run(self) -> Result<()> {
+        let server_addr = "127.0.0.1:6007".parse().unwrap();
+        let (server_config, _server_cert) = self.configure_server().unwrap();
+        let (endpoint, mut incoming) = Endpoint::server(server_config, server_addr)?;
+        println!("remote_conn_svc running on {}", endpoint.local_addr()?);
 
-    let (server_config, _server_cert) = configure_server().unwrap();
-    let (endpoint, mut incoming) = Endpoint::server(server_config, server_addr)?;
-    println!("listening on {}", endpoint.local_addr()?);
-
-    while let Some(conn) = incoming.next().await {
-        info!("connection incoming");
-        let fut = handle_connection(conn);
-        tokio::spawn(async move {
+        while let Some(conn) = incoming.next().await {
+            info!("remote client connecting from {}", conn.remote_address());
+            let fut = self.handle_connection(conn);
             if let Err(e) = fut.await {
                 error!("connection failed: {}", e.to_string())
             }
-        });
+        }
+
+        Ok(())
     }
 
-    Ok(())
-}
+    #[allow(clippy::field_reassign_with_default)]
+    fn configure_server(&self) -> Result<(ServerConfig, Vec<u8>), Box<dyn Error>> {
+        let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+        let cert_der = cert.serialize_der().unwrap();
+        let priv_key = cert.serialize_private_key_der();
+        let priv_key = rustls::PrivateKey(priv_key);
+        let cert_chain = vec![rustls::Certificate(cert_der.clone())];
 
-#[allow(clippy::field_reassign_with_default)]
-fn configure_server() -> Result<(ServerConfig, Vec<u8>), Box<dyn Error>> {
-    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-    let cert_der = cert.serialize_der().unwrap();
-    let priv_key = cert.serialize_private_key_der();
-    let priv_key = rustls::PrivateKey(priv_key);
-    let cert_chain = vec![rustls::Certificate(cert_der.clone())];
+        let mut server_config = ServerConfig::with_single_cert(cert_chain, priv_key)?;
+        Arc::get_mut(&mut server_config.transport)
+            .unwrap()
+            .max_concurrent_uni_streams(0_u8.into())
+            .keep_alive_interval(std::time::Duration::new(5,0).into());
 
-    let mut server_config = ServerConfig::with_single_cert(cert_chain, priv_key)?;
-    Arc::get_mut(&mut server_config.transport)
-        .unwrap()
-        .max_concurrent_uni_streams(0_u8.into());
-    
-    Ok((server_config, cert_der))
-}
+        Ok((server_config, cert_der))
+    }
 
-async fn handle_connection(conn: quinn::Connecting) -> Result<()> {
-    let quinn::NewConnection {connection: _, mut bi_streams, ..} = conn.await?;
-    async {
-        println!("established");
-        while let Some(stream) = bi_streams.next().await {
-            let stream = match stream {
-                Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
-                    println!("connection closed");
-                    return Ok(());
-                }
-                Err(e) => {
-                    return Err(e);
-                }
-                Ok(s) => s,
-            };
-            let fut = handle_request(stream);
-            tokio::spawn(
-                async move {
-                    if let Err(e) = fut.await {
-                        error!("failed: {}", e.to_string());
+    async fn handle_connection(&self, conn: quinn::Connecting) -> Result<()> {
+        let quinn::NewConnection {connection: _, mut bi_streams, ..} = conn.await?;
+
+        async {
+            println!("established");
+            while let Some(stream) = bi_streams.next().await {
+                let stream = match stream {
+                    Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                        println!("connection closed");
+                        return Ok(());
                     }
+                    Err(e) => {
+                        return Err(e);
+                    }
+                    Ok(s) => s,
+                };
+                let fut = self.handle_request(stream);
+                if let Err(e) = fut.await {
+                    error!("failed: {}", e.to_string());
                 }
-            );
-        }
+            }
+            Ok(())
+        }.await?;
+
         Ok(())
-    }.await?;
-    Ok(())
-}
+    }
 
-async fn handle_request((mut send, recv): (quinn::SendStream, quinn::RecvStream)) -> Result<()> {
-    let req = recv.read_to_end(64 * 1024).await.map_err(|e| anyhow!("failed reading request: {}", e))?;
-    let pkt = decode(req);
-    let resp = process_packet(pkt).unwrap_or_else(|e| {
-        error!("failed: {}", e);
-        format!("failed to process request: {}\n", e).into_bytes()
-    });
+    async fn handle_request(&self, (mut send, recv): (quinn::SendStream, quinn::RecvStream)) -> Result<()> {
+        let req = recv.read_to_end(64 * 1024).await.map_err(|e| anyhow!("failed reading request: {}", e))?;
+        let pkt = decode(req);
+        let resp = self.process_packet(pkt).unwrap_or_else(|e| {
+            error!("failed: {}", e);
+            format!("failed to process request: {}\n", e).into_bytes()
+        });
 
-    send.write_all(&resp).await.map_err(|e| anyhow!("failed to send response: {}", e))?;
-    send.finish().await.map_err(|e| anyhow!("failed to shutdown stream: {}", e))?;
-    info!("complete!");
+        send.write_all(&resp).await.map_err(|e| anyhow!("failed to send response: {}", e))?;
+        send.finish().await.map_err(|e| anyhow!("failed to shutdown stream: {}", e))?;
+        info!("complete!");
 
-    Ok(())
-}
+        Ok(())
+    }
 
-fn process_packet(mut pkt: Packet) -> Result<Vec<u8>> {
-    pkt.timestamp = std::time::SystemTime::now();
-    println!("{:?}", pkt.payload);
-    let res = encode(pkt);
-
-    Ok(res)
+    fn process_packet(&self, mut pkt: Packet) -> Result<Vec<u8>> {
+        pkt.timestamp = std::time::SystemTime::now();
+        println!("{:?}", pkt.payload);
+        let res = encode(pkt);
+    
+        Ok(res)
+    }
 }
