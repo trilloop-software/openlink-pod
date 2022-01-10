837d365390495df2b003841db58665c38b082232
Axat <abhagat5@uwo.ca>
2022-01-10 17:15:26 -0500

    brake and launch connection complete

--- src/launch_svc.rs
+++ src/launch_svc.rs
@@ -19,6 +19,7 @@ impl LaunchSvc {
         loop {
             tokio::select!{ // mmight have to add brake svc
                 _val = self.rx_auth.recv() => {
+                    println!("auth -> launch received");
                     let response_ = Packet {
                         packet_id: s!["OPENLINK"],
                         version: 1,
@@ -29,6 +30,21 @@ impl LaunchSvc {
                     };
 
                     if let Err(e) = self.tx_auth.send(response_).await{
+                        eprintln!("Launch->Auth failed: {}", e);
+                    }
+                }
+
+                _val = self.rx_brake.recv() => {
+                    println!("brake -> launch received");
+                    let response_ = Packet {
+                        packet_id: s!["OPENLINK"],
+                        version: 1,
+                        cmd_type: 65,
+                        timestamp: std::time::SystemTime::now(),
+                        payload: vec!["Launch".to_string()]
+                    };
+
+                    if let Err(e) = self.tx_brake.send(response_).await{
                         eprintln!("Launch->Brake failed: {}", e);
                     }
                 }
